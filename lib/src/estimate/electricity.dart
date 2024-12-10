0cdebfe9da2feaf32bf5726b00521a6308049c67
Gabritorre <gabritorredev@gmail.com>
2024-12-10 12:23:34 +0100

    Refactor electricity estimation UI code, fix dropdown menu

--- lib/src/estimate/electricity.dart
+++ lib/src/estimate/electricity.dart
@@ -21,6 +21,7 @@ class _ElectricityEstimationViewState extends State<ElectricityEstimationView> {
 	final TextEditingController countryController = TextEditingController();
 
 	void estimateElectricity(electricityValue, country) {
+		print('Estimating electricity $electricityValue kWh in $country');
 		WidgetsBinding.instance.addPostFrameCallback((_) {
 			Api.fetchElectricityEstimates(
 				electricityValue: electricityValue,
@@ -56,24 +57,10 @@ class _ElectricityEstimationViewState extends State<ElectricityEstimationView> {
 								children: [
 									const SizedBox(height: 10),
 									Padding(
-										padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-										child: TextFormField(
-											keyboardType: TextInputType.number,
-											decoration: const InputDecoration(
-												labelText: 'Electricity consumption',
-												border: UnderlineInputBorder(),
-												suffix : Text(
-													'kWh',
-													textAlign: TextAlign.center,
-													style: TextStyle(
-														fontSize: 18,
-														fontWeight: FontWeight.bold,
-														color: Color.fromARGB(255, 155, 155, 155),
-													),
-												),
-											),
+										padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+										child: ElectricityConsumptionField(
 											validator: (value) {
-												if (value == null || value.isEmpty) {
+											if (value == null || value.isEmpty) {
 													return 'Field required';
 												}
 												electricityValue = double.tryParse(value);
@@ -83,33 +70,42 @@ class _ElectricityEstimationViewState extends State<ElectricityEstimationView> {
 									),
 									const SizedBox(height: 20),
 									Padding(
-										padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-										child: DropdownMenu(
+										padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+										child: CountrySelectorField(
 											controller: countryController,
-											width: double.infinity,
-											enableFilter: true,
-											enableSearch: false,
-											requestFocusOnTap: true,
-											label: const Text('Country'),
 											onSelected: (String? country) {
 												selectedCountry = country;
 											},
-											dropdownMenuEntries: Broker.getCountries().map(Option.asDropdownMenuEntry).toList(),
-										),
+										)
 									),
-									ElevatedButton(
-										onPressed: () {
-											if ((_formKey.currentState != null && !_formKey.currentState!.validate()) || selectedCountry == null ) {
-												ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Invalid form')));
-											}
-											else {
-												setState(() {
-													estimate = null;
-												});
-												estimateElectricity(electricityValue, selectedCountry);
-											}
-										},
-										child: const Text('Estimate'),
+									Padding(
+										padding: const EdgeInsets.symmetric(vertical: 15),
+										child: ElevatedButton(
+											style: ElevatedButton.styleFrom(
+												backgroundColor: const Color.fromARGB(255, 61, 61, 61), // Colore di sfondo
+												foregroundColor: const Color.fromARGB(255, 223, 223, 223), // Colore del testo
+												padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 14.0), // Personalizza il padding
+											),
+											onPressed: () {
+												if ((_formKey.currentState != null && !_formKey.currentState!.validate()) || selectedCountry == null ) {
+													ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Invalid form')));
+												}
+												else {
+													setState(() {
+														estimate = null;
+													});
+													estimateElectricity(electricityValue, selectedCountry);
+												}
+											},
+											child: const Text(
+												'Estimate',
+												textAlign: TextAlign.center,
+												style: TextStyle(
+													fontSize: 18,
+													fontWeight: FontWeight.bold,
+												),
+											),
+										),
 									),
 								],
 							),
@@ -118,20 +114,81 @@ class _ElectricityEstimationViewState extends State<ElectricityEstimationView> {
 							builder: (context) {
 								if (error != null) {
 									return Text(error!);
-								} else if (estimate != null) {
-									return Text('$estimate $carbonUnit');
+								}
+								else if (estimate != null) {
+									return Padding(
+										padding: const EdgeInsets.symmetric(vertical: 15),
+										child:Text(
+											'$estimate $carbonUnit of CO2',
+											textAlign: TextAlign.center,
+											style: const TextStyle(
+												fontSize: 23,
+												fontWeight: FontWeight.bold,
+												color: Color.fromARGB(255, 212, 212, 212),
+											),
+										),
+									);
 								} 
 								else if (estimate == null && selectedCountry == null && electricityValue == null) {
 									return const SizedBox(height: 0);
 								}
 								else {
-									return const CircularProgressIndicator();
+									return const Padding(
+										padding: EdgeInsets.symmetric(vertical: 15),
+										child: CircularProgressIndicator()
+									);
 								}
 							},
 						),
 					],
 				),
 			),
+		);
+	}
+}
+
+class ElectricityConsumptionField extends StatelessWidget {
+	const ElectricityConsumptionField({super.key, this.validator});
+	final FormFieldValidator<String>? validator;
+
+	@override
+	Widget build(BuildContext context) {
+				return TextFormField(
+			keyboardType: TextInputType.number,
+			decoration: const InputDecoration(
+				labelText: 'Electricity consumption',
+				border: UnderlineInputBorder(),
+				suffix : Text(
+					'kWh',
+					textAlign: TextAlign.center,
+					style: TextStyle(
+						fontSize: 18,
+						fontWeight: FontWeight.bold,
+						color: Color.fromARGB(255, 155, 155, 155),
+					),
+				),
+			),
+			validator: validator,
+		);
+	}
+}
+
+class CountrySelectorField extends StatelessWidget{
+	const CountrySelectorField({super.key, this.controller, this.onSelected});
+	final TextEditingController? controller;
+	final ValueChanged<String?>? onSelected;
+	@override
+	Widget build(BuildContext context) {
+		return DropdownMenu(
+			controller: controller,
+			width: double.infinity,
+			menuHeight: 200,
+			enableFilter: true,
+			enableSearch: false,
+			requestFocusOnTap: true,
+			label: const Text('Country'),
+			onSelected: onSelected,
+			dropdownMenuEntries: Broker.getCountries().map(Option.asDropdownMenuEntry).toList(),
 		);
 	}
 }
